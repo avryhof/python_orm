8cde8b6d66957a37416506879867d3b03511f062
Amos Vryhof <amos@vryhofresearch.com>
2020-03-11 13:04:11 -0400

    Ignore settings files.  Merge back helpers.

--- helpers.py
+++ helpers.py
@@ -1,9 +1,7 @@
+import pprint
 import struct
 
 import dateutil
-
-
-import pprint
 
 
 def get_val(target_object, key, default_value=None):
@@ -23,9 +21,7 @@ def get_val(target_object, key, default_value=None):
 
 def handle_datetimeoffset(dto_value):
     # ref: https://github.com/mkleehammer/pyodbc/issues/134#issuecomment-281739794
-    tup = struct.unpack(
-        "<6hI2h", dto_value
-    )  # e.g., (2017, 3, 16, 10, 35, 18, 0, -6, 0)
+    tup = struct.unpack("<6hI2h", dto_value)  # e.g., (2017, 3, 16, 10, 35, 18, 0, -6, 0)
     tweaked = [tup[i] // 100 if i == 6 else tup[i] for i in range(len(tup))]
 
     retn = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}.{:07d} {:+03d}:{:02d}".format(*tweaked)
